87bfcf880cafe803a1b1842b1c5cd84b05d1e50d
Sa4ok <sasha.rusinka@gmail.com>
2024-10-10 17:36:09 +0300

    mobile-ready

--- js/script.js
+++ js/script.js
@@ -1,16 +1,40 @@
 function toggleMenu() {
-  const menu = document.getElementById('menu');
-  if (menu.style.display === 'flex') {
-    menu.style.display = 'none';
-  } else {
-    menu.style.display = 'flex';
-  }
+	const menu = document.getElementById('menu');
+	if (menu.style.display === 'flex') {
+		menu.style.display = 'none';
+	} else {
+		menu.style.display = 'flex';
+	}
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+	var buttons = document.querySelectorAll('.menu-link');
+
+	buttons.forEach(function (button) {
+		button.addEventListener('click', function (event) {
+			// Prevent the default link behavior
+			event.preventDefault();
+
+			// Remove highlight from all buttons
+			buttons.forEach(function (btn) {
+				btn.style.backgroundColor = '';
+				btn.style.color = '';
+			});
+
+			// Highlight the clicked button
+			event.target.style.backgroundColor = 'black';
+			event.target.style.borderRadius = '22px';
+			event.target.style.color = 'white';
+		});
+	});
+});
+
 // Button arrow-down scroll
-document.getElementById('scrollButton').addEventListener('click', function() {
-  window.scrollBy({
-      top: window.innerHeight,
-      behavior: 'smooth'
-  });
+document.querySelectorAll('.scrollButton').forEach((button) => {
+	button.addEventListener('click', function () {
+		window.scrollBy({
+			top: window.innerHeight,
+			behavior: 'smooth',
+		});
+	});
 });
